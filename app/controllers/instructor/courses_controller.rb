73a44f63785c7d1ed7ba4be515c44fb30558963c
Dane Schrank <danerrr@mail.com>
2019-07-25 23:56:34 +0000

    added more validations

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -27,6 +27,11 @@ class Instructor::CoursesController < ApplicationController
     end
   end
 
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
   def course_params
     params.require(:course).permit(:title, :description, :cost)
   end
